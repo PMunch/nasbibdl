e53273b19ead7e23da5d3bdc2db47d53fcc5a20c
PMunch <peterme@peterme.net>
2026-01-01 21:07:45 +0100

    Add resize parameter as option

--- download.sh
+++ download.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 if [ -z "$1" ] || [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
-  echo "Usage: download.sh <URN of book>"
+  echo "Usage: download.sh <URN of book> [<resize parameters>]"
   echo ""
   echo "Downloads a book from Nasjonalbiblioteket, turns the pages into black"
-  echo "and white, collates it into a PDF, and runs OCR on it. Currently"
-  echo "hard-coded to my e-readers size, should be an optional argument. The"
-  echo "name of the PDF is the name of the book. After the book is downloaded"
-  echo "you can use addtoc.sh and addmeta.sh to add metadata and a table of"
-  echo "contents to the PDF."
+  echo "and white, collates it into a PDF, and runs OCR on it. The name of the"
+  echo "PDF is the name of the book. If the second resize parameter is given it"
+  echo "will be passed to imagemagicks -resize flag. It is recommended to"
+  echo "resize the PDF to a size which matches the physical resolution of the"
+  echo "reader either with the \"Fill area\" flag, ie. '1072x1488^' (notice the"
+  echo "caret at the end), or with only the width ie. '1072x'. This means the"
+  echo "image will be scaled to at least fill the display or at least fill the"
+  echo "width respectively. Most scans will be larger than this, so doing the"
+  echo "resize step saves both space in the resulting PDF but also saves the"
+  echo "reader from having to do the resizing. More complex operations like"
+  echo "trimming large white margins is left as an excercise for the reader but"
+  echo "are certainly possible. After the book is downloaded you can use"
+  echo "addtoc.sh and addmeta.sh to add metadata and a table of contents to"
+  echo "the PDF."
   exit 1
 fi
 
@@ -16,6 +25,10 @@ META=$(curl "https://api.nb.no/catalog/v1/items/$1")
 TITLE=$(echo "$META" | jq -r '.metadata.title')
 PAGES=$(echo "$META" | jq '.metadata.pageCount')
 URN=$(echo "$META" | jq -r '.metadata.identifiers.urn')
+RESIZE="$2"
+if [ -n "$RESIZE" ]; then
+  RESIZE="-resize $RESIZE"
+fi
 
 echo "Downloading $PAGES from book \"$TITLE\""
 
@@ -24,10 +37,10 @@ dezoomify-rs "https://www.nb.no/services/image/resolver/${URN}_C3/info.json" -l 
 dezoomify-rs "https://www.nb.no/services/image/resolver/${URN}_I1/info.json" -l "I1.jpg"
 dezoomify-rs "https://www.nb.no/services/image/resolver/${URN}_I3/info.json" -l "I3.jpg"
 
-magick "C1.jpg" -resize 1072x1448 "C1-compress.png"
-magick "C3.jpg" -resize 1072x1448 "C3-compress.png"
-magick "I1.jpg" -resize 1072x1448 "I1-compress.png"
-magick "I3.jpg" -resize 1072x1448 "I3-compress.png"
+magick "C1.jpg" $RESIZE "C1-compress.png"
+magick "C3.jpg" $RESIZE "C3-compress.png"
+magick "I1.jpg" $RESIZE "I1-compress.png"
+magick "I3.jpg" $RESIZE "I3-compress.png"
 rm "C1.jpg"
 rm "C3.jpg"
 rm "I1.jpg"
@@ -38,7 +51,7 @@ echo $TEXTPAGES
 for i in $(seq 1 $TEXTPAGES);
 do
   dezoomify-rs "https://www.nb.no/services/image/resolver/${URN}_$(printf '%04d' "$i")/info.json" -l "page-$(printf '%04d' "$i").jpg"
-  magick "page-$(printf '%04d' "$i").jpg" -resize 1072x1448 -colorspace gray -auto-level -level 10%,90% +dither -remap "palette-5.png" "page-$(printf '%04d' "$i")-compress.png"
+  magick "page-$(printf '%04d' "$i").jpg" $RESIZE -colorspace gray -auto-level -level 10%,90% +dither -remap "palette-5.png" "page-$(printf '%04d' "$i")-compress.png"
   rm "page-$(printf '%04d' "$i").jpg"
 done
 
